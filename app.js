d3a5e6321c5a75fd6a2e3dfebb9c14e44de2a139
KiranDulam <kiransaichandra@gmail.com>
2020-12-04 11:34:50 -0500

    made changes to app.js file for adding consolelogs in app1.get to check the workflow

--- app.js
+++ app.js
@@ -8,10 +8,13 @@ const app1= express1();
 app1.use(bodyparser1.urlencoded({extended: true}));
 
 app1.get("/",function(req1,res1){
+     console.log("function got the path and ran app1.get. Next has to run sendFile");
      res1.sendFile(__dirname+"/index.html");
+     console.log("function got the path and ran app1.get. res1.sendFile completed.");
+
     }
      );
 
-const port= process.env.port || 3305;
+const port= process.env.PORT || 3305;
 
-app1.listen(port,function(){ console.log("App1 server started on port Env port or 3304");});
+app1.listen(port,function(){ console.log("App1 server started on port Env port or 3305");});
