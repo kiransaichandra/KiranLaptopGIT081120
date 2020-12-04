00f4a22bc9964f48e6ca299dcaada05da0e9b032
KiranDulam <kiransaichandra@gmail.com>
2020-12-04 10:36:49 -0500

    Added npm configuration and npm init and installed npm packages: express and body-parser to set this up on AWS Elastic beanstack with Github for this location

---
+++ app1.js
@@ -0,0 +1,17 @@
+// Deploying this on aws Elastic Beanstack with Github for this folder
+
+const express1= require("express");
+const bodyparser1=require("body-parser");
+
+const app1= express1();
+
+app1.use(bodyparser1.urlencoded({extended: true}));
+
+app2.get("/",function(req1,res1){
+     res1.sendFile(__dirname+"/index.html");
+    }
+     );
+
+const port= process.env.port || 3304;
+
+app2.listen(port,function(){ console.log("App1 server started on port Env port or 3304");});
