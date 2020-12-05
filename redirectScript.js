09db5bb7f9ebdf3d1d3b3e406a67056fd10be59d
KiranDulam <kiransaichandra@gmail.com>
2020-12-05 10:58:20 -0500

    updated redirectjs file, as both aws and github pages are using two different redirect js files

--- redirectScript.js
+++ redirectScript.js
@@ -1,1 +1,3 @@
-setInterval(()=>{location.replace("ProjectUdemyWebDevelopment1/HTML Content1/HTMLIndex1.html");},100);
+setInterval(()=>{location.replace("ProjectUdemyWebDevelopment1_StaticFolder1/HTML Content1/HTMLIndex1.html");},100);
+
+//ProjectUdemyWebDevelopment1_StaticFolder1/HTML Content1/HTMLIndex1.html
